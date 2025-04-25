2dfe9577f515d3252a5599146cadba45af0c4080
amilai <amila.wedikkara@gmail.com>
2025-04-25 15:30:33 +0300

    Improve login interface

--- routes/auth.js
+++ routes/auth.js
@@ -17,9 +17,9 @@ router.post(
   ],
   async (req, res) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty())
+    if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
-
+    }
     try {
       const { email, password } = req.body;
       const hashedPassword = await hashPassword(password);
